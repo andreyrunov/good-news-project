cb6ef49f83783e59c8784a5607c1c266f0c45727
Andrey Runov <elefant86@bk.ru>
2022-05-25 19:43:47 +0300

    can get data from ria.ru

--- routes/catalogRouter.js
+++ routes/catalogRouter.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const axios = require('axios');
+const { parseString } = require('xml2js');
 const { User, Category } = require('../db/models');
 
 router.route('/')
@@ -6,10 +8,22 @@ router.route('/')
     const id = 1;
     const userName = await User.findOne({ where: { id } });
     // console.log(userName);
-    
+
     const categoryNames = await Category.findAll();
-    console.log(categoryNames);
-    
+    // console.log(categoryNames);
+
+    const getRiaNews = await axios.get('https://ria.ru/export/rss2/archive/index.xml');
+    // const toJson = getRiaNews.json();
+    // const newResult =  parseString(String(getRiaNews.data), (err, result) => result);
+    // result.rss.channel[0].item
+    // console.log(newResult);
+    let news;
+    parseString(getRiaNews.data, (err, result) => {
+      news = result.rss.channel[0].item;
+    });
+    console.log(news.map((el) => ({ title: el.title, description: el.description, enclosure: el.enclosure?.[0].$ || null })));
+    // console.log(Object.keys(getRiaNews));
+
     res.render('catalog', { userName, categoryNames });
   });
 
