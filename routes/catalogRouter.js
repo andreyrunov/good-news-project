9f8ef97ad0cf0548cab6c1586afc221218e1ed2d
Andrey Runov <elefant86@bk.ru>
2022-05-25 18:33:46 +0300

    add side menu, name to navbar

--- routes/catalogRouter.js
+++ routes/catalogRouter.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const { User, Category } = require('../db/models');
 
 router.route('/')
-  .get((req, res) => {
-    res.render('catalog');
+  .get(async (req, res) => {
+    const id = 1;
+    const userName = await User.findOne({ where: { id } });
+    // console.log(userName);
+    
+    const categoryNames = await Category.findAll();
+    console.log(categoryNames);
+    
+    res.render('catalog', { userName, categoryNames });
   });
 
 module.exports = router;
